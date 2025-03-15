282d19a95b4def3f0ec963ec61d71e8762a53599
dr-rompecabezas <felavid@gmail.com>
2025-03-15 16:21:47 -0400

    refactor: Update static files storage configuration to use new STORAGES format

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files
 if DEBUG:
