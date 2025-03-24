7920fb1afabebcc47114a48287cb040949addeda
dr-rompecabezas <felavid@gmail.com>
2025-03-24 01:00:13 -0400

    Add URL route for retrieving SVG content of glyphs

--- apps/writing/urls.py
+++ apps/writing/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("practice/<str:glyph_name>/", views.practice, name="practice"),
     path("check-drawing/", views.check_drawing, name="check_drawing"),
+    path("svg/<str:glyph_name>/", views.get_svg_content, name="get_svg_content"),
 ]
