d67bd3288acca4cbbb9c7a5dee730b48155af8ea
dr-rompecabezas <felavid@gmail.com>
2025-03-30 22:12:16 -0400

    feat: enhance ASGI configuration to support WebSocket connections with authentication

--- config/asgi.py
+++ config/asgi.py
@@ -9,21 +9,25 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+# Import after initializing Django
+from apps.tutor import routing  # noqa: E402
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # "websocket": AuthMiddlewareStack(
-        #     URLRouter(
-        #         routing.websocket_urlpatterns
-        #     )
-        # ),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        ),
     }
 )
